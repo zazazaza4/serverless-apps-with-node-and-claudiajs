365731a8748372a98977ff639150512fe53c5a4c
Yaroslav <6715414@gmail.com>
2023-11-06 11:47:07 +0200

    feat: add routes put /orders, delete /orders, post /orders

--- pizza-api/api.js
+++ pizza-api/api.js
@@ -1,7 +1,10 @@
 const Api = require('claudia-api-builder');
 const api = new Api();
 
-const getPizzas = require('./handlers/get-pizzas');
+const getPizzas = require('./handlers/get-pizzas.js');
+const createOrder = require('./handlers/create-order.js');
+const deleteOrder = require('./handlers/delete-order.js');
+const updateOrder = require('./handlers/update-order.js');
 
 api.get('/', () => 'Welcome to Pizza API');
 
@@ -13,11 +16,43 @@ api.get(
   '/pizzas/{id}',
   (request) => {
     const pizzaId = request.pathParams.id;
-
     return getPizzas(pizzaId);
   },
   {
     error: 404,
+  }
+);
+
+api.post(
+  '/orders',
+  (request) => {
+    return createOrder(request.body);
+  },
+  {
+    success: 201,
+    error: 400,
+  }
+);
+
+api.put(
+  '/orders/{id}',
+  (request) => {
+    const id = request.pathParams.id;
+    return updateOrder(id, request.body);
+  },
+  {
+    error: 400,
+  }
+);
+
+api.delete(
+  '/orders/{id}',
+  (request) => {
+    const id = request.pathParams.id;
+    return deleteOrder(id);
+  },
+  {
+    error: 400,
   }
 );
 
